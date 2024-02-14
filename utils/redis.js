c1bc3ee0ec0746ed0a3d992349c191b7ae80e4e4
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-02-14 08:32:19 +0100

    added utils folder

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.client.on('error', (err) => console.log(err));
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        const value = await this.getAsync(key);
+        return value;
+    }
+
+    async set(key, value, duration) {
+        this.client.setex(key, duration, value);
+    }
+
+    async del(key) {
+        this.client.del(key);
+    }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
